c3851d31b04fe22cd1a34136af981c7c9dd84a1b
Can Ince <can@ince.io>
2025-10-03 09:09:03 +0100

    Bump

--- wasm/Main.cpp
+++ wasm/Main.cpp
@@ -99,7 +99,7 @@ public:
     val postMessageBatch (val payload)
     {
         auto v = emValToValue(payload);
-
+        NATIVE_LOG_INFO("wasm", "postMessageBatch: %s", v.toString().c_str());
         if (!v.isArray()) {
             return valueToEmVal(elem::js::Object {
                 {"success", false},
